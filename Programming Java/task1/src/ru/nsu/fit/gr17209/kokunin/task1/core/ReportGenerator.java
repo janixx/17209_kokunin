96a07d97b71a3ae80798b13aa9bfcafb35e7adce
janixx <n.kokunin@g.nsu.ru>
2019-04-14 20:10:18 +0700

    remove old version of .java-files

--- Programming Java/task1/src/ru/nsu/fit/gr17209/kokunin/task1/core/ReportGenerator.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.fit.gr17209.kokunin.task1.core;
-
-import java.util.List;
-
-public interface ReportGenerator {
-    List<ReportRow> getReport();
-}
--- Programming Java/task1/src/ru/nsu/fit/gr17209/kokunin/task1/core/ReportWriter.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.fit.gr17209.kokunin.task1.core;
-
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.util.List;
-
-public interface ReportWriter {
-    void writeReport(List<ReportRow> reportList) throws IOException;
-}
